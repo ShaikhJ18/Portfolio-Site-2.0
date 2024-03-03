9786b17fd5263bfe9b4895b2496497549ce7fc9c
ShaikhJ18 <J.shk1980@gmail.com>
2024-03-03 14:33:15 -0500

    fixed some minor bugs

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,22 +9,24 @@ const HeroSection = () => {
             <div className="col-span-7 place-self-center text-center sm:text-left">
             <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"><span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-400 ">Hello, I'm{" "}</span> 
     <br></br><TypeAnimation
-      sequence={[
+    sequence={[
         // Same substring at the start will only be typed out once, initially
         'Jawad Shaikh',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        1000,
         'an Aspiring Software Engineer',
         1000,
         'a Problem Solver',
         1000,
         'an Innovator',
         1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '1em', display: 'inline-block' }}
-      repeat={Infinity}
-    />
+    ]}
+    wrapper="span"
+    speed={50}
+    style={{ fontSize: '1em', display: 'inline-block' }}
+    repeat={Infinity}
+/>
+
+
             </h1>
             <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             I am a second year Computer Science student at the University of Guelph minoring in Mathematics.
@@ -35,11 +37,12 @@ const HeroSection = () => {
             </div>
             </div>
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                <Image src="/images/myface2.0.jpeg"
-                    alt="myFaceImage"
-                    className="rounded-full bg-[#181818] w-[200px] h-[200px] lg:w-[400px] lg:h-[400px]"
-                    width={300}
-                    height={300}/>
+            <Image
+            src="/images/myface2.0.jpeg"
+            alt="myFaceImage"
+            className="rounded-full bg-[#181818] w-[200]"
+            />
+
             </div>
             </div>
 
